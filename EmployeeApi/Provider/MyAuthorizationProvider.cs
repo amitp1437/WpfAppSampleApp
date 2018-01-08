dd02aeadf07b50f2fee68dd25bd09a0322a75488
deopan <deonandanpandit@gmail.com>
2018-01-08 17:49:33 +0530

    Added some files to EmployeeApi project

---
+++ EmployeeApi/Provider/MyAuthorizationProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace EmployeeApi.Provider
+{
+    public class MyAuthorizationProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if (context.UserName == "admin" && context.Password == "admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Sheikh Imran"));
+                context.Validated(identity);
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "DeoNandan Pandit"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("Grant_Error", "Invalid UserName or Password");
+                return;
+            }
+        }
+    }
+}
